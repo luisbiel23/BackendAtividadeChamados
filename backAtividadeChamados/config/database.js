67be1a205147397e237f397b7903962c4150e74e
luisbiel23 <101661799+luisbiel23@users.noreply.github.com>
2025-12-04 20:13:36 -0300

    Add files via upload

---
+++ backAtividadeChamados/config/database.js
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // necessário para Supabase
+    },
+  },
+});
+
+export default sequelize;
