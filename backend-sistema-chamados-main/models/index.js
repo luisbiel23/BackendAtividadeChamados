e39172dd613eacd65b5c6a8aef769af8a0d11121
luisbiel23 <101661799+luisbiel23@users.noreply.github.com>
2025-12-04 20:12:18 -0300

    Delete backend-sistema-chamados-main directory

--- backend-sistema-chamados-main/models/index.js
+++
@@ -1,7 +0,0 @@
-import Usuario from './Usuario.js';
-import Chamado from './Chamado.js';
-
-Usuario.hasMany(Chamado, { foreignKey: 'id_usuario', as: 'chamados' });
-Chamado.belongsTo(Usuario, { foreignKey: 'id_usuario', as: 'usuario' });
-
-export { Usuario, Chamado };
